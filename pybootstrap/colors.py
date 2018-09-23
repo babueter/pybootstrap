3cc2459177d9eca31c9964503a4847ceee4d7f48
Bryan Bueter <bryan@bueterfamily.org>
2018-09-23 14:21:56 -0400

    Add colors attributes

---
+++ pybootstrap/colors.py
@@ -0,0 +1,24 @@
+from .core import Attribute
+
+
+TEXT_PRIMARY = Attribute("class", "text-primary")
+TEXT_SECONDARY = Attribute("class", "text-secondary")
+TEXT_SUCCESS = Attribute("class", "text-success")
+TEXT_DANGER = Attribute("class", "text-danger")
+TEXT_WARNING = Attribute("class", "text-warning")
+TEXT_INFO = Attribute("class", "text-info")
+TEXT_LIGHT = Attribute("class", "text-light")
+TEXT_DARK = Attribute("class", "text-dark")
+TEXT_MUTED = Attribute("class", "text-muted")
+TEXT_WHITE = Attribute("class", "text-white")
+
+BG_PRIMARY = Attribute("class", "p-3 mb-2 bg-primary")
+BG_SECONDARY = Attribute("class", "p-3 mb-2 bg-secondary")
+BG_SUCCESS = Attribute("class", "p-3 mb-2 bg-success")
+BG_DANGER = Attribute("class", "p-3 mb-2 bg-danger")
+BG_WARNING = Attribute("class", "p-3 mb-2 bg-warning")
+BG_INFO = Attribute("class", "p-3 mb-2 bg-info")
+BG_LIGHT = Attribute("class", "p-3 mb-2 bg-light")
+BG_DARK = Attribute("class", "p-3 mb-2 bg-dark")
+BG_MUTED = Attribute("class", "p-3 mb-2 bg-muted")
+BG_WHITE = Attribute("class", "p-3 mb-2 bg-white")
