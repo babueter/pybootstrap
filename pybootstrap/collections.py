05e373b0712540108559790da6bb91920b55e938
Bryan Bueter <bryan@bueterfamily.org>
2018-09-23 12:28:46 -0400

    Change private methods to public names

--- pybootstrap/collections.py
+++ pybootstrap/collections.py
@@ -18,26 +18,25 @@ class Table(Container):
         self.add_rows(rows)
         self.add_cols(cols)
 
-    def _rows(self) -> int:
+    def rows(self) -> int:
         return len(self.components)
 
     def add_rows(self, rows: int):
-        row_index = self._rows() + 1
+        row_index = self.rows() + 1
         for row in range(rows):
             index = row_index + row
             row_component = Container(self.ROW_TAG, id="{}_row_{}".format(self.id, index))
             self.add_component(row_component)
 
-            for col in range(self._cols()):
+            for col in range(self.cols()):
                 col_component = Container(self.COL_TAG, id="{}_row_{}_col_{}".format(self.id, index, col+1))
                 row_component.add_component(col_component)
-
 
     def get_row(self, row: int) -> Container:
         return self.get_component("{}_row_{}".format(self.id, row))
 
-    def _cols(self) -> int:
-        if self._rows():
+    def cols(self) -> int:
+        if self.rows():
             row = self.get_row(1)
             if row:
                 return len(row.components)
@@ -45,10 +44,10 @@ class Table(Container):
         return 0
 
     def add_cols(self, cols: int):
-        col_index = self._cols() + 1
+        col_index = self.cols() + 1
         for col in range(cols):
             index = col_index + col
-            for row in range(self._rows()):
+            for row in range(self.rows()):
                 row_component = self.get_row(row+1)
                 if row_component:
                     col_component = Container(self.COL_TAG, id="{}_row_{}_col_{}".format(self.id, row+1, index))
