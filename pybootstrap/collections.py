b67083ed3b9699f2857f7a4f7fc8e26d00063a05
Bryan Bueter <bryan@bueterfamily.org>
2018-10-06 10:39:02 -0400

    Add Carousel component

--- pybootstrap/collections.py
+++ pybootstrap/collections.py
@@ -2,7 +2,8 @@
 HTML Component Collections
 """
 
-from pybootstrap.core import Container
+from pybootstrap.core import Container, Component
+from .utils import add_class_attributes
 
 
 class ProgressBar(Container):
@@ -89,3 +90,86 @@ class Table(Container):
 
     def get_col(self, row, col):
         return self.get_component("{}_row_{}_col_{}".format(self.id, row, col))
+
+
+class Carousel(Container):
+    def __init__(self, controls=False, indicators=False, interval=5000, keyboard=True, pause_on_hover=False, ride=False, wrap=True, **attributes):
+        attributes["class"] = "carousel slide"
+        super().__init__("div", **attributes)
+
+        self.indicators = indicators
+        self.interval = interval
+        self.keyboard = keyboard
+        self.ride = ride
+        self.wrap = wrap
+
+        self.add_attributes(**{"data-interval": str(interval)})
+        self.add_attributes(**{"data-keyboard": str(keyboard).lower()})
+        self.add_attributes(**{"data-ride": str(ride).lower()})
+        self.add_attributes(**{"data-wrap": str(wrap).lower()})
+
+        if pause_on_hover:
+            self.add_attributes(**{"data-pause": "hover"})
+
+        if indicators:
+            self.indicators = Container("ol", **{"class": "carousel-indicators"})
+            self.add_component(self.indicators)
+
+        self.inner = Container("div", **{"class": "carousel-inner"})
+        self.add_component(self.inner)
+
+        if controls:
+            control_prev = Container(
+                "a", href="#carouselExampleIndicators", role="button",
+                **{
+                    "class": "carousel-control-prev",
+                    "data-slide": "prev",
+                },
+            )
+            control_prev.add_component(Component("span", inline=True, **{"class": "carousel-control-prev-icon", "aria-hidden": "true"}))
+            control_prev.add_component(Component("span", inline=True, **{"class": "sr-only"}, text="Previous"))
+
+            control_next = Container(
+                "a", href="#carouselExampleIndicators", role="button",
+                **{
+                    "class": "carousel-control-next",
+                    "data-slide": "next",
+                },
+            )
+            control_next.add_component(Component("span", inline=True, **{"class": "carousel-control-next-icon", "aria-hidden": "true"}))
+            control_next.add_component(Component("span", inline=True,**{"class": "sr-only"}, text="Next"))
+
+            self.add_component(control_prev)
+            self.add_component(control_next)
+
+    def add_item(self, item: Component):
+        item_index = str(len(self.inner.components))
+        if self.indicators:
+            # <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+            indicator = Component(
+                "li", inline=True,
+                **{
+                    "data-target": "#carouselIndicators",
+                    "data-slide-to": item_index,
+                },
+            )
+            if item_index == "0":
+                add_class_attributes(indicator, "active")
+
+            self.indicators.add_component(indicator)
+
+        item_container = Container("div", **{"class": "carousel-item"})
+        if item_index == "0":
+            add_class_attributes(item_container, "active")
+
+        item_container.add_component(item)
+        self.inner.add_component(item_container)
+
+    def __str__(self):
+        self.get_attribute("data-interval").values = set((str(self.interval), ))
+        self.get_attribute("data-keyboard").values = set((str(self.keyboard).lower(), ))
+        self.get_attribute("data-ride").values = set((str(self.ride).lower(), ))
+        self.get_attribute("data-wrap").values = set((str(self.wrap).lower(), ))
+
+        return super().__str__()
+
